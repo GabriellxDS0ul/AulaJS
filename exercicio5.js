9d458946e3d31259f54a75ed0135db9d44f7ed47
GabriellxDS0ul <dojacat2109@gmail.com>
2026-03-10 11:05:04 -0300

    10.03.2026

---
+++ exercicio5.js
@@ -0,0 +1,5 @@
+document.write("<h1> Aula 01 - Exercício 5</h1>")
+
+var lado = Number(prompt("Digite o lado do quadrado:"));
+var area = lado **2
+document.write("<p> A area do quadrado é:" +  area + "</p>");
